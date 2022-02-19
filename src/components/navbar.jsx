e4684caaabb50ee430a0a0c8f1f12264b75c2fe7
LAPTOP-FENL63J0\PL <PL@gmail.com>
2022-02-19 17:59:11 +0800

    :zap: Updated Nav Bar to show total count

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,7 +5,7 @@ class NavBar extends Component {
       <nav class="navbar navbar-light bg-light">
         <div class="container-fluid">
           <a class="navbar-brand" href="#">
-            Navbar{""} <span className="badge badge-pill badge-secondary"></span>
+            Navbar{""} <span className="badge badge-pill bg-secondary">{this.props.totalCounters}</span>
           </a>
         </div>
       </nav>
