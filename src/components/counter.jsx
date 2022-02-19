b83ba596dc440c24495439087598483f044b726c
LAPTOP-FENL63J0\PL <PL@gmail.com>
2022-02-19 10:30:51 +0800

    :heavy_plus_sign: Half of the lecture
    1. Making a commit here to preserve useful concepts/implementations that are taught inside of the lecture.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,19 +2,41 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    imageUrl: 'https://picsum.photos/200'
+    imageUrl: 'https://picsum.photos/200',
+    tags: ["tag1","tag2","tag3"]
   };
-
+  // constructor(){
+  //   super();
+  //   console.log('Constructor',this);
+  //   this.handleIncrement = this.handleIncrement.bind(this)
+  // }
   styles = {
       fontSize: 50,
       fontWeight: "bold"
   }
+  renderTags(){
+    if(this.state.tags.length === 0 ) return <p>There are no tags!</p>;
+    return <ul>{this.state.tags.map(tag=><li key={tag}>{tag}</li>)}</ul>
+  }
+  handleIncrement = (product) => {
+    console.log(product)
+    console.log('Increment Clicked',this)
+    this.state.count++;
+    this.setState({count:this.state.count+1})//Must use this to notify react of the changes
+  }
+
+  doHandleIncrement = () =>{
+    this.handleIncrement({id: 1})
+  }
+
   render() {
     return (
       <React.Fragment>
         <img src = {this.state.imageUrl} alt = ""/>
         <span style={{fontSize: 20}} className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btm-sm">Increment</button>
+        <button onClick={()=>this.handleIncrement({hid:7})} className="btn btn-secondary btm-sm">Increment</button>{/*Passing the reference instead of calling the method. Different from vanila js */}
+        {this.state.tags.length ===0 && 'create a new tag'}
+        <div>{this.renderTags()}</div>
       </React.Fragment>
     );
   }
